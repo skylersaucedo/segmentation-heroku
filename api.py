f549b7b272726c3e55e68b77539cc1b501c1f27b
skylersaucedo <skyler.saucedo@gmail.com>
2022-12-13 11:44:05 -0600

    inital commit

---
+++ api.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI, File, UploadFile
+import tensorflow as tf
+import json
+from model_definition import SegmentationModel
+
+
+app = FastAPI()
+
+model = SegmentationModel().model
+model.load_weights('UNET_256x256_20nov_2022_final_weights.h5')
+
+@app.post('/')
+
+async def scoring_endpoint(data: UploadFile = File(...)):
+    image_bytes = await data.read()
+    image = tf.io.decode_image(image_bytes)
+    yhat = model.predict(tf.expand_dims(image, axis=0))
+
+    return {"prediction": json.dumps(yhat.tolist())}
